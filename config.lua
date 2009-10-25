e0430720cdce9fe6aefe607322ab709c2696361e
STPeters <stefan@chehalispost.com>
2009-10-25 12:10:13 -0700

    ﻿sample config script

--- config.lua
+++ config.lua
@@ -1,6 +1,7 @@
 -- configuration script for luafcgid
-workers = 5 	-- number of worker threads
-states = 10		-- number of Lua VM states
-sweep = 10000	-- housekeeping sweep cycle in milliseconds
-retries = 3		-- number of search cycles before creating
-				-- a new Lua VM state for a requested script
+
+workers = 5    -- number of worker threads
+states = 10    -- number of Lua VM states
+sweep = 10000  -- housekeeping sweep cycle in milliseconds
+retries = 3    -- number of search cycles before creating
+               -- a new Lua VM state for a requested script
