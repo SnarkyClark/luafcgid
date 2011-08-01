c07efdc22b22adc7eb217cc0385fb61099759cb9
STPeters <stefan@chehalispost.com>
2011-07-31 19:43:03 -0700

    cleanup

--- config.lua
+++ config.lua
@@ -1,19 +1,20 @@
 --[[ configuration script for luafcgid ]]--
 
 -- port or socket path to listen to
-listen = ":9000"
+-- listen = "127.0.0.1:9000"
+listen = "/var/tmp/luafcgid.socket"
 
 -- number of worker threads
 workers = 3
 
--- max number of Lua VM states
-states = 5
+-- max number of Lua VM states (NOTE: value needs to be larger then or equal to "workers")
+states = workers * 2
 
 -- number of clones of each script allowed
 clones = states
 
 -- housekeeping sweep cycle in milliseconds
-sweep = 1000
+sweep = 5000
 
 -- number of search cycles before creating
 -- a new Lua VM state for a requested script
