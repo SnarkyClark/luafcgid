b5e88575f96fd5c534d1decf00262ac7517e640c
STPeters <stefan@chehalispost.com>
2011-08-01 10:46:32 -0700

    update example

--- examples/dumpenv.lua
+++ examples/dumpenv.lua
@@ -1,15 +1,29 @@
 function main(env, req)
-	s = {}
-
-	table.insert(s, "<pre>\n")
-	for n, v in pairs(env) do
-		table.insert(s, string.format("%s = %s\n", n, v))
-	end
-	table.insert(s, "</pre>\n")
+	--req:header('Status: 404 Not Found\r\n')
+	req:header({
+		['Status'] = '200 OK',
+		['Content-Type'] = 'text/html'
+	})
 	
-	req:puts(table.concat(s))
+	req:puts("<h1>Environment</h1><pre>\n")
+	for n, v in pairs(env) do
+		req:puts(string.format("%s = %s\n", n, v))
+	end
+	req:puts("</pre>\n")
+
+	if env.REQUEST_METHOD == "GET" then
+		s = {}  -- string sets are MUCH faster then calling req:puts() all the time
+		params = req:parse(env.QUERY_STRING)
+		table.insert(s, "<h1>GET Params</h1><pre>\n")
+		for n, v in pairs(params) do
+			table.insert(s, string.format("%s = %s\n", n, v))
+		end
+		table.insert(s, "</pre>\n")
+		req:puts(table.concat(s))
+	end
 
 	if env.REQUEST_METHOD == "POST" then
+		req:puts("<h1>POST</h1>\n")
 		req:puts(string.format("<textarea>%s</textarea>", req:gets()))
 	end
 
