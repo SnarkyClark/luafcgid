a4440c587a87d6fe5b4e07f2e12861038a4967b7
STPeters <stefan@chehalispost.com>
2011-08-01 10:32:46 -0700

    forgot header line

--- request.h
+++ request.h
@@ -15,6 +15,7 @@ void luaL_pushrequest(lua_State* L, request_t* r);
 
 extern const struct luaL_Reg request_methods[];
 
+int req_header(lua_State *L);
 int req_puts(lua_State *L);
 int req_gets(lua_State *L);
 int req_parse(lua_State *L);
