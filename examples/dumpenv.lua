eb49ee80d20fb413fc7571a1fc225a5e262a290f
Stefan Peters <stefan@chehalispost.com>
2011-10-08 12:38:17 -0700

    update sample script

--- examples/dumpenv.lua
+++ examples/dumpenv.lua
@@ -1,28 +1,29 @@
-local fcgi = require("luafcgid")
+-- utilies for processing a FCGI request
+local lf = require("luafcgid")
 
-function main(env, req)
+function main(env, con)
 	-- gotta give Lua some props
-	req:header("X-Powered-By", "Lua")
+	con:header("X-Powered-By", "Lua")
 	
-	req:puts("<h1>Environment</h1><pre>\n")
+	con:puts("<h1>Environment</h1><pre>\n")
 	for n, v in pairs(env) do
-		req:puts(string.format("%s = %s\n", n, v))
+		con:puts(string.format("%s = %s\n", n, v))
 	end
-	req:puts("</pre>\n")
+	con:puts("</pre>\n")
 
 	if env.REQUEST_METHOD == "GET" then
 		s = {}  -- string sets are faster then calling req:puts() all the time
-		params = fcgi.parse(env.QUERY_STRING)
+		params = lf.parse(env.QUERY_STRING)
 		table.insert(s, "<h1>GET Params</h1><pre>\n")
 		for n, v in pairs(params) do
 			table.insert(s, string.format("%s = %s\n", n, v))
 		end
 		table.insert(s, "</pre>\n")
-		req:puts(table.concat(s))
+		con:puts(table.concat(s))
 	end
 
 	if env.REQUEST_METHOD == "POST" then
-		req:puts("<h1>POST</h1>\n")
-		req:puts(string.format("<textarea>%s</textarea>", req:gets()))
+		con:puts("<h1>POST</h1>\n")
+		con:puts(string.format("<textarea>%s</textarea>", con:gets()))
 	end
 end
